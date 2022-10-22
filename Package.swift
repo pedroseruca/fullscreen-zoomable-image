f936220db3bd02032cb64e5499ad608bee25424e
Pedro Seruca <pedroseruca@gmail.com>
2022-10-22 17:22:18 +0100

    Fixed package.swift file

--- Package.swift
+++ Package.swift
@@ -20,10 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ZoomableImage",
-            path: "Sources"),
-	    exclude: [
+            path: "Sources",
+            exclude: [
                 "zoomable_contentApp.swift",
                 "Preview Content"
-            ],
+            ]),
     ]
 )
